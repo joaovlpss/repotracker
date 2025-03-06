e19c2c024fffa2f0ae12e9fa382f3610370c8b77
joaovlpss <joaovlpssdev@gmail.com>
2025-03-06 15:42:48 -0300

    Updated gitignore. Fixed init methods in database_orm.

--- src/objects/database_orm.py
+++ src/objects/database_orm.py
@@ -3,17 +3,27 @@ from typing import List, Optional
 
 
 class StaffOrm:
-    id: int
-    name: str
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
 
 
 class CommitOrm:
-    id: Optional[int] = None
-    author_id: int
-    branch_id: int
-    comment: str
-    date: datetime
-    file_changes: int
+    def __init__(
+        self,
+        author_id: int,
+        branch_id: int,
+        comment: str,
+        date: datetime,
+        file_changes: int,
+        id: Optional[int] = None,
+    ):
+        self.id = id
+        self.author_id = author_id
+        self.branch_id = branch_id
+        self.comment = comment
+        self.date = date
+        self.file_changes = file_changes
 
     class Config:
         json_encoders = {
@@ -22,18 +32,33 @@ class CommitOrm:
 
 
 class BranchOrm:
-    id: Optional[int] = None
-    name: str
-    repo_id: int
-    commits: List[CommitOrm] = []
+    def __init__(
+        self,
+        name: str,
+        repo_id: int,
+        commits: List[CommitOrm] | None = None,
+        id: Optional[int] = None,
+    ):
+        self.id = id
+        self.name = name
+        self.repo_id = repo_id
+        self.commits = commits if commits is not None else []
 
 
 class RepositoryOrm:
-    id: Optional[int] = None
-    name: str
-    author_id: int
-    last_commit_date: datetime
-    branches: List[BranchOrm] = []
+    def __init__(
+        self,
+        name: str,
+        author_id: int,
+        last_commit_date: datetime,
+        branches: List[BranchOrm] | None = None,
+        id: Optional[int] = None,
+    ):
+        self.id = id
+        self.name = name
+        self.author_id = author_id
+        self.last_commit_date = last_commit_date
+        self.branches = branches if branches is not None else []
 
     class Config:
         json_encoders = {

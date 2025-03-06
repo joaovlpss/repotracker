c0d90d7973c53016dc99323f8065700ddbf0ce78
joaovlpss <joaovlpssdev@gmail.com>
2025-03-06 15:18:36 -0300

    Added get_resource_from_config to main.py, in order to use config.toml

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import sqlite3
 import os
+import tomllib
 from src.objects.RepositoryManager import RepositoryManager
 from src.objects.DataGatherer import DataGatherer
 
 
+def get_resource_from_config(key: str, value: str):
+    """
+    Use the config.toml file to get any resource, provided
+    that the key-value pair exists.
+    """
+
+    with open("./config.toml", "rb") as config:
+        config = tomllib.load(config)
+
+    # First, check if key-value pair exists.
+    if not config[key][value]:
+        raise ValueError("Key-value pair doest not exist in config.toml.")
+
+    return config[key][value]
+
+
 def main():
-    db_file_path = "./database/repodb.sqlite3"
-    repo_json_path = "./tracked_repos/repositories.json"
+    db_file_path = get_resource_from_config("database", "locale")
+    repo_json_path = get_resource_from_config("json", "locale")
+    csv_dump_path = get_resource_from_config("csv_dump", "locale")
 
     repo_manager = RepositoryManager(db_file_path, repo_json_path)
 
@@ -15,7 +32,7 @@ def main():
         repo_manager.update_repository(repo)
         print(f"Updated repository: {os.path.basename(repo.working_dir)}")
 
-    data_gatherer = DataGatherer()
+    data_gatherer = DataGatherer(db_file_path, csv_dump_path)
 
     # Dump all commits to a CSV file
     data_gatherer.dump_commits("commits_dump.csv")
--- src/objects/DataGatherer.py
+++ src/objects/DataGatherer.py
@@ -1,42 +1,15 @@
 import pandas as pd
 import sqlite3
-import tomllib
-
-from pydantic import BaseModel, ConfigDict
 
 
-class DataGatherer(BaseModel):
+class DataGatherer:
     db_connection: sqlite3.Connection
     dump_path: str
     query_result: pd.DataFrame
 
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True
-    )  # Allow arbitrary types for sqlite3 Connection.
-
-    def __init__(self):
-        self.db_connection = self.get_connection()
-        self.dump_path = self.get_dump_path()
-
-    def get_connection(self) -> sqlite3.Connection:
-        """
-        Use the config.toml file to initialize the database connection.
-        """
-        with open("./config.toml", "rb") as config:
-            config = tomllib.load(config)
-
-        db_path: str = config["database"]["locale"]
-        return sqlite3.connect(db_path)
-
-    def get_dump_path(self) -> str:
-        """
-        Use the config.toml file to initialize the database connection.
-        """
-        with open("./config.toml", "rb") as config:
-            config = tomllib.load(config)
-
-        dump_path: str = config["csv_dump"]["locale"]
-        return dump_path
+    def __init__(self, db_path: str, dump_path: str):
+        self.db_connection = sqlite3.connect(db_path)
+        self.dump_path = dump_path
 
     def dump_commits(self, file_name: str) -> None:
         """
